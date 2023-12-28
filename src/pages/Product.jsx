9eb67671cbb7286448aabb51ff2190b6534cd9dc
Joachim Mwengi <kimwambua96@gmail.com>
2023-12-29 01:20:54 +0300

    added basic styling to individual product page

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -16,14 +16,20 @@ const ProductPage = () => {
   const { imgURL, name, price } = product;
 
   return (
-    <div className="container mx-auto mt-8">
+    <div className="container mx-auto">
       <div className="flex">
-        <div className="w-1/2">
-          <img src={imgURL} alt={name} className="w-full h-auto" />
+        <div className="w-1/2 mt-40">
+          <img
+            src={imgURL}
+            alt={name}
+            className="ml-40 mb-20 h-auto"
+            width={540}
+            height={540}
+          />
         </div>
-        <div className="w-1/2 pl-8">
+        <div className="w-1/2 pl-8 mt-40">
           <h2 className="text-3xl font-semibold">{name}</h2>
-          <p className="text-lg text-gray-700 mb-4">{price}</p>
+          <p className="text-lg text-gray-700 mb-8">{price}</p>
           <p className="text-gray-600">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
             facilisi. Proin nec nunc nec nisi varius fermentum. Fusce at urna ac
